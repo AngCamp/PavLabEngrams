45271f3c002aae460d0a606c2382c96136e1241e
Angus Campbell <angus.campbell@ubc.ca>
2022-01-07 15:09:13 -0800

    added metadata for several studies, classifier.r has been modified though it is still garbage, collated data in an xcsl file

--- EngramCellClassifier/classifier.r
+++ EngramCellClassifier/classifier.r
@@ -50,11 +50,38 @@ chen2020_meta <- read.csv( 'Chen2020_GSE152632/SraRunTable.txt', header = TRUE)
 # https://www.ncbi.nlm.nih.gov/pmc/articles/PMC6079101/
 #gds_jeager2018 <- getGEO("GSE98679")
 jeager2018_counts <- read.table('Jeager2018_GSE98679/GSE98679_count.txt.gz', header = TRUE, check.names = FALSE)
+jeager2018_v2_counts <- read.table('Jeager2018_GSE98679/GSE98679_v2_GSM3308862-GSM3309413_count.txt.gz', header = TRUE, check.names = FALSE)
+
+
+#> which(rownames(jeager2018_counts)=="Arc")
+#[1] 3021
+
+#> which(rownames(jeager2018_counts)=="Fos")
+#[1] 8233
+
+#> which(rownames(jeager2018_counts)=="Prox1")
+#[1] 35227
 
 jeager2018_v2_tpm <- read.table('Jeager2018_GSE98679/GSE98679_v2_GSM3308862-GSM3309413_tpm.txt.gz')
-jeager_meta <- read.csv('Jeager2018_GSE98679/SraRunTable.txt', header = TRUE)
+jeager2018_meta <- read.csv('Jeager2018_GSE98679/SraRunTable.txt', header = TRUE)
+
+#meta data is poorly detailed, but essentially in exposure the v2 counts are described
+
 #note that some of the meta data, specifically the stain is in the cell barcodes in the count data
 
+
+#lacar2016
+lacar2016_meta <- read.csv('Lacar2016_GSE77067/SraRunTable.txt', header = TRUE)
+lacar2016_wc_counts <- read.table('Lacar2016_GSE77067/GSE77067_wc_counts.txt.gz')
+lacar2016_snHC_counts <- read.table('Lacar2016_GSE77067/GSE77067_sn_hc_counts.txt.gz')
+lacar2016_snNE_counts <- read.table('Lacar2016_GSE77067/GSE77067_sn_ne_counts.txt.gz')
+
+
+#Cho et al., 2016
+cho2016_data <- read.table('Cho2016_GSE85128/GSE85128_SuppTable1_normalized_read_counts_resubmission.xls.gz')
+
+#Rao-Ruiz
+rao2019_meta=read.csv('Rao-Ruiz2019_GSE129024/SraRunTable.txt', header=TRUE)
 
 #in the meta data the source_name columns should allow for merging, though
 # the colnames in jeager2018_counts may need to be parsed
