f6e03aa5fe7beaf4ebec6534961a560e499809a1
Angus Campbell <angus.campbell@ubc.ca>
2022-01-07 21:46:25 -0800

    minor changes to classifier, moved scripts to fetchiData

--- EngramCellClassifier/classifier.r
+++ EngramCellClassifier/classifier.r
@@ -10,18 +10,17 @@ library(tidyverse)
 library(GEOquery)
 library(AnnotationDbi)
 
-
-#jeager et al., (2018) meta data and counts
-jeager2018_counts <- read.table('Jeager2018_GSE98679/GSE98679_count.txt.gz', header = TRUE, check.names = FALSE)
-jeager2018_v2_counts <- read.table('Jeager2018_GSE98679/GSE98679_v2_GSM3308862-GSM3309413_count.txt.gz', header = TRUE, check.names = FALSE)
-
-jeager2018_meta <- read.csv('Jeager2018_GSE98679/SraRunTable.txt', header = TRUE)
-
-
 #Lacar et al., (2016)
 lacar2016_meta <- read.csv('Lacar2016_GSE77067/SraRunTable.txt', header = TRUE)
 lacar2016_snHC_counts <- read.table('Lacar2016_GSE77067/GSE77067_sn_hc_counts.txt.gz')
 lacar2016_snNE_counts <- read.table('Lacar2016_GSE77067/GSE77067_sn_ne_counts.txt.gz')
 
 
+
+#Jeager et al., (2018) meta data and counts
+jeager2018_counts <- bind_cols(read.table('Jeager2018_GSE98679/GSE98679_count.txt.gz', header = TRUE, check.names = FALSE),
+                               read.table('Jeager2018_GSE98679/GSE98679_v2_GSM3308862-GSM3309413_count.txt.gz', header = TRUE, check.names = FALSE))
+
+jeager2018_meta <- read.csv('Jeager2018_GSE98679/SraRunTable.txt', header = TRUE)
+jeager2018_meta = jeager2018_meta[c(1:46,599:912,47:598),] #we need to fix Jeager's files up a bit
 
