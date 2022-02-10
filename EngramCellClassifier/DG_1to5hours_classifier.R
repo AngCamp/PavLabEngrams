9ebfad64c5c0774779c1f6628346a72524c269e2
Angus Campbell <angus.campbell@ubc.ca>
2022-02-09 19:46:53 -0800

    classifeirs for the different engram cells

---
+++ EngramCellClassifier/DG_1to5hours_classifier.R
@@ -0,0 +1,150 @@
+#
+# first attempt at a classifier
+setwd("C:/Users/angus/Desktop/PavLabEngrams/EngramCellClassifier")
+
+library(tidyverse)
+library(GEOquery)
+library(AnnotationDbi)
+library(randomForest)
+library(data.table)
+library(reshape2)
+library(FactoMineR)
+library(factoextra)
+library(Rtsne)
+library(dplyr)
+library(Seurat)
+library(stringr)
+library(patchwork)
+library(metap)
+
+
+
+# Lacar and Jeager --------------------------------------------------------
+
+
+# Lacar et al., (2016)
+lacar2016_meta <- read.csv('Lacar2016_GSE77067/SraRunTable.txt', header = TRUE)
+lacar2016_snHC_counts <- read.table('Lacar2016_GSE77067/GSE77067_sn_hc_counts.txt.gz')
+lacar2016_snNE_counts <- read.table('Lacar2016_GSE77067/GSE77067_sn_ne_counts.txt.gz')
+
+
+
+# Jeager et al., (2018) meta data and counts
+jeager2018_counts <- bind_cols(read.table('Jeager2018_GSE98679/GSE98679_count.txt.gz', header = TRUE, check.names = FALSE),
+                               read.table('Jeager2018_GSE98679/GSE98679_v2_GSM3308862-GSM3309413_count.txt.gz', header = TRUE, check.names = FALSE))
+
+jeager2018_meta <- read.csv('Jeager2018_GSE98679/SraRunTable.txt', header = TRUE)
+jeager2018_meta = jeager2018_meta[c(1:46,599:912,47:598),] #we need to fix Jeager's files up a bit
+rownames(jeager2018_meta) <- c(1:912)
+
+
+# blank.to.DG <-function(x){
+#   if(x==""){
+#     x="DG"
+#   }
+# }
+
+jeager2018_meta$predicted_cell_type <- as.character(lapply(jeager2018_meta$predicted_cell_type, function(x) if (x=="") {"DG"} else {x}))
+jeager2018_meta$predicted_cell_type <- lapply(jeager2018_meta$predicted_cell_type, function(x) if (x=="") {"DG"} else {x})
+
+#Finding engram cells
+fospos <- which(grepl("_F_DG",  jeager2018_meta$source_name))
+fospos <- c(fospos,361:912) # since we know all the v2 cells from time points and recall testing
+
+neg <- which(grepl("_N_DG",  jeager2018_meta$source_name))
+
+jeager2018_meta$fos_status <- as.factor(sapply(as.character(jeager2018_meta$source_name), function(y) if (grepl("_F_", y, fixed=TRUE)) "Fos+" else "Fos-"  ))
+
+#filtering out cells as per instructions in Jeager et al., 2018)
+under4k <- sapply(jeager2018_counts, function(y) sum(length(which(y>0))))
+
+filtered.idx <- as.numeric(which(under4k>4000))
+filtered.idx <- order(c(filtered.idx,194))
+
+jeager2018_counts <-jeager2018_counts[,filtered.idx]
+jeager2018_meta <- jeager2018_meta[filtered.idx,]
+
+
+#Make seurate object
+jeager2018_counts[is.na(jeager2018_counts)] = 0
+jeager2018 <- CreateSeuratObject(counts = log(jeager2018_counts+1))
+
+jeager2018 <- FindVariableFeatures(jeager2018, selection.method = "vst", nfeatures = 3530) #no idea how I chose 3530, 
+# in the tutorial they chose 2000, I just chose to match the number of cells we had
+#jeager2018nobatch <- ScaleData(jeager2018)
+
+rownames(jeager2018_meta) <- colnames(jeager2018_counts)
+jeager2018 <- AddMetaData(jeager2018, jeager2018_meta)
+
+
+
+
+#Writting the Classifier: https://www.rdocumentation.org/packages/Seurat/versions/2.3.4/topics/ClassifyCells
+
+
+
+#First we do integration, find anchors etc
+
+#split the data by experiment 
+
+# D0 not do now but I should be trying to recreate the resutls of the 
+# DEG lists from the original authors and trygint to restrict the classifier to those
+# genes  Suerat has a tutorial on getting the DEGs out of it. https://satijalab.org/seurat/articles/de_vignette.html
+
+
+
+# Following this we can run the data through this function
+ClassifyCells(jeagerdata, classifier, training.genes = NULL,
+              training.classes = NULL, new.data = test.set, ...)
+
+
+# Next steps?  What would be a good positive control?
+
+
+
+
+
+
+### DG dataset to check:
+# Hochgerner, H., Zeisel, A., Lönnerberg, P., & Linnarsson, S. (2018). 
+# Conserved properties of dentate gyrus neurogenesis across postnatal 
+# development revealed by single-cell RNA sequencing. Nature neuroscience, 
+# 21(2), 290-299.
+#GEO Accession
+#https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE95753
+#meta data on SRA
+# https://www.ncbi.nlm.nih.gov/Traces/study/?acc=PRJNA378266&o=acc_s%3Aa
+~32000 cells, NOTE THE DATASET IS TOO LARGE DOWNLOAD IT LATER
+
+
+Zhang, T. Y., Keown, C. L., Wen, X., Li, J., Vousden, D. A., Anacker, C., ... & Meaney, M. J. (2018).
+Environmental enrichment increases transcriptional and epigenetic differentiation
+between mouse dorsal and ventral dentate gyrus. Nature communications, 9(1), 1-11.
+
+-methylation data but may be interestingto provide some context to our results,
+worth reading
+
+
+
+# Habib, N., Li, Y., Heidenreich, M., Swiech, L., Avraham-Davidi, I., 
+# Trombetta, J. J., ... & Regev, A. (2016). Div-Seq: Single-nucleus RNA-Seq reveals
+# dynamics of rare adult newborn neurons. Science, 353(6302), 925-928.
+# https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE84371
+
+# data here: https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE85721
+
+test_habib2016_logTPM <- read.table("test_datasets/Habib2016/GSE85721_PROCESSED_data.sNuc-Seq_Data_RSEM_log_TPM.for_GEO.txt.gz", header = TRUE)
+rownames(test_habib2016_logTPM) <- test_habib2016_logTPM[, 1]  ## set rownames
+test_habib2016_logTPM <- test_habib2016_logTPM[, 2:925]   
+#note that based on the colnames there appear to be some CA1 and other cells in here,
+#as well as some conditions, old young etc that will need parsing, read the pub
+# then get on it
+condition_label <- colnames(test_habib2016_logTPM)
+sum(str_detect(colnames(test_habib2016_logTPM), ".DG"))
+# [1] 545 pf DG cells not a bad number  of cells to test on
+
+
+test_habib2016_meta <- read.csv("test_datasets/Habib2016/SraRunTable.txt", header =TRUE, fill =TRUE)
+
+
+
